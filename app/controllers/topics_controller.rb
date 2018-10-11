f8f1362793cb2a7ce7afab828e1df671535e67b1
m.taka <taozig8@gmail.com>
2018-10-11 06:27:06 +0000

    BBSindex&new-ok

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,18 +1,23 @@
 class TopicsController < ApplicationController
+  
+  def new
+    
+  end 
+    
   def index
     @topics = Topic.all
-    @newTopic = Topic.new
+    @topic = Topic.new
   end
 
   def create
     @topic = Topic.new(params[:topic].permit(:title))
     @topic.save
-    redirect_to topics_index_path
+    redirect_to topics_path
   end
   
   def delete
     @topic = Topic.find(params[:id])
     @topic.destroy
-    redirect_to topics_index_path
+    redirect_to topics_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
   get 'history', to: 'history#show'
   resources :shops
   
-  get 'topics/index'
-  get 'topics/show/:id' => 'topics#show', as: :topics_show
-  #’topics/show/:id’へのアクセスは、tpicsコントローラのshowメソッドが受け取る。
-  #３行目みたいなgetto構文じゃダメなのかな？
+  resources :topics
   
-  post 'topics/create' => 'topics#create'
-  delete 'topics/delete/:id' => 'topics#delete', as: :topic_delete
 end
